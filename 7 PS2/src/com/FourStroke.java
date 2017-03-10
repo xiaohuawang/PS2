4f22d9cd76694e48aaefe6b9210656863dc2197d
xiaohua <wxhdax@gmail.com>
2017-03-09 23:11:30 -0500

    Second

---
+++ 7 PS2/src/com/FourStroke.java
@@ -0,0 +1,20 @@
+package com;
+
+public class FourStroke extends Engine {
+
+	public FourStroke(int id, double volume, double fuelCapacity, char fuelType) {
+		super(id, volume, fuelCapacity, fuelType);
+	}
+
+	@Override
+	public double getMileage() {
+		double mileage=0.0;
+		if(fuelType=='g'){
+			mileage = volume*4*3/1000  + fuelCapacity/2;
+		}else if(fuelType=='d'){
+			mileage = volume*4*2/1000  + fuelCapacity/4;
+		}
+		return mileage;
+	}
+
+}
---
+++ 7 PS2/src/com/NoEngineExistException.java
@@ -0,0 +1,16 @@
+package com;
+
+public class NoEngineExistException extends Exception{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public String getMessage(){
+		return "No Engine Exists With Greater Mileage&quot";
+	}
+	
+	
+}
